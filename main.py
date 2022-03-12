53f7ae7fe6b12c0ff2356ad9b1ed16be3ecba00e
KyleTelnes <59575950+KyleTelnes@users.noreply.github.com>
2022-03-11 21:14:09 -0800

    Added Get_Max_Credits function & clarifying comments

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import igraph
 # re is used for validation
 import re
 
-# 
+'''
+Function: Validate_Input
+Description:    
+    Checks the contents of the file line by line using a
+    regular expression to make sure that each line is formatted correctly.
+Parameters: 
+    filename - the name of the file to validate
+Returns: 
+    True - if each line has the correct format
+    False - if there is a line with incorrect formatting
+'''
 def Validate_Input(filename):
     f = open(filename, "r")
     for line in f:
@@ -11,13 +21,49 @@ def Validate_Input(filename):
         if x == None or x.start() != 0:
             return False
     return True
-            
+
+'''
+Function: Get_Max_Credits
+Description:    
+    Asks the user for the maximum amount of credits that they
+    desire per quarter
+Parameters: 
+    None
+Returns: 
+    max_credits - the maximum number of credits
+'''
+def Get_Max_Credits():
+    credits = 18
+    while True:
+        try:
+            credits = int(input("Please enter the maximum number of credits per quarter: "))
+        # Make sure that the input is a number
+        except ValueError:
+            print("Not a valid value.")
+            continue
+        # Validate the input
+        if credits < 0:
+            print("The value of credits cannot be negative.")
+            continue
+        if credits > 18:
+            print("The value of credits cannot exceed 18.")
+            continue
+        else:
+            break
+    return credits
 
 def main():
+    # Step 2: Validate Input
     if Validate_Input("test.txt") != False:
         print("File Format is Valid!")
     else:
         print("File Format is Not Valid!")
+
+    # Step 4: Ask User for Maximum Credits
+    max_credits = Get_Max_Credits()
+
+    print(max_credits)
+    
     #dg = igraph.Graph(directed=True)
     
     #dg.add_vertices(5)
